03bd42a72e7fda21456b92b8264ecd0f19b97d29
hoho0507 <2251061893@e.tlu.edu.vn>
2025-06-21 15:43:06 +0700

    product va home

--- app/src/main/java/vn/edu/tlu/group23/mybakeryapp/activities/HomeActivity.java
+++ app/src/main/java/vn/edu/tlu/group23/mybakeryapp/activities/HomeActivity.java
@@ -1,9 +1,14 @@
 package vn.edu.tlu.group23.mybakeryapp.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button; // Vẫn giữ nếu bạn có các nút khác
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView; // Thêm import này
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -12,11 +17,59 @@ import vn.edu.tlu.group23.mybakeryapp.R;
 
 public class HomeActivity extends AppCompatActivity {
 
+    private ImageView ivLogout;
+    private CardView cardManageProducts; // Thay đổi từ Button sang CardView
+    private CardView cardManageEmployees; // Thay đổi từ Button sang CardView
+    private CardView cardManageTasks;     // Thay đổi từ Button sang CardView
+    private CardView cardStatistics;      // Thay đổi từ Button sang CardView
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_home);
 
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main_home), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        // Ánh xạ View (sử dụng ID của CardView)
+        ivLogout = findViewById(R.id.ivLogout);
+        cardManageProducts = findViewById(R.id.cardManageProducts); // Ánh xạ CardView
+        cardManageEmployees = findViewById(R.id.cardManageEmployees); // Ánh xạ CardView
+        cardManageTasks = findViewById(R.id.cardManageTasks);         // Ánh xạ CardView
+        cardStatistics = findViewById(R.id.cardStatistics);          // Ánh xạ CardView
+
+        // Xử lý sự kiện click cho các CardView
+        cardManageProducts.setOnClickListener(v -> {
+            Intent intent = new Intent(HomeActivity.this, ProductActivity.class);
+            startActivity(intent);
+        });
+
+        cardManageEmployees.setOnClickListener(v -> {
+            Toast.makeText(HomeActivity.this, "Chức năng Quản lý Nhân viên!", Toast.LENGTH_SHORT).show();
+            // TODO: Chuyển đến EmployeeManagementActivity
+        });
+
+        cardManageTasks.setOnClickListener(v -> {
+            Toast.makeText(HomeActivity.this, "Chức năng Quản lý Công việc !", Toast.LENGTH_SHORT).show();
+            // TODO: Chuyển đến TaskManagementActivity
+        });
+
+        cardStatistics.setOnClickListener(v -> {
+            Toast.makeText(HomeActivity.this, "Chức năng Thống kê & Báo cáo sẽ được phát triển sau!", Toast.LENGTH_SHORT).show();
+            // TODO: Chuyển đến StatisticsActivity
+        });
+
+        ivLogout.setOnClickListener(v -> {
+            Toast.makeText(HomeActivity.this, "Đăng xuất thành công!", Toast.LENGTH_SHORT).show();
+            finish();
+            // Nếu có LoginActivity:
+            // Intent intent = new Intent(AdminHomeActivity.this, LoginActivity.class);
+            // intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            // startActivity(intent);
+        });
     }
 }
--- app/src/main/java/vn/edu/tlu/group23/mybakeryapp/database/DatabaseHelper.java
+++ app/src/main/java/vn/edu/tlu/group23/mybakeryapp/database/DatabaseHelper.java
@@ -4,9 +4,12 @@ import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
+import androidx.annotation.NonNull;
+
+import vn.edu.tlu.group23.mybakeryapp.models.Product;
 public class DatabaseHelper extends SQLiteOpenHelper {
     public static final String DATABASE_NAME = "Bakery.db";
-    public static final int DATABASE_VERSION = 1;
+    public static final int DATABASE_VERSION = 2;
     public DatabaseHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
@@ -17,11 +20,14 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         // Thêm tài khoản mẫu để test
         db.execSQL("INSERT INTO Employee (username, password, fullname, phone, email) " +
                 "VALUES ('admin', '123456', 'Quản trị hệ thống', '0123456789', 'admin@bakery.com')");
+        //tạo bảng sản phẩm
+        createProductTable(db);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         db.execSQL("DROP TABLE IF EXISTS Employee");
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_SANPHAM);
         onCreate(db);
     }
 
@@ -38,4 +44,31 @@ public class DatabaseHelper extends SQLiteOpenHelper {
                 ")";
         db.execSQL(createEmployeeTable);
     }
+    public static final String TABLE_SANPHAM = "sanpham";
+
+    // Tên cột
+    public static final String COL_HINHANH = "hinhAnh";
+    public static final String COL_MASP = "maSP";
+    public static final String COL_TENSP = "tenSP";
+    public static final String COL_GIABAN = "giaBan";
+    public static final String COL_LOAIBAHN = "loaiBanh";
+    public static final String COL_MOTA = "moTa";
+    public static final String COL_SOLUONGDABAN = "soLuongDaBan";
+    public static final String COL_HINHTHUCBAN = "hinhThucBan";
+
+    public void createProductTable(@NonNull SQLiteDatabase db) {
+        String sql = "CREATE TABLE " + TABLE_SANPHAM + " (" +
+                COL_HINHANH + " TEXT," +
+                COL_MASP + " TEXT PRIMARY KEY," +
+                COL_TENSP + " TEXT," +
+                COL_GIABAN + " INTEGER," +
+                COL_LOAIBAHN + " TEXT," +
+                COL_MOTA + " TEXT," +
+                COL_SOLUONGDABAN + " INTEGER," +
+                COL_HINHTHUCBAN + " TEXT" +
+                ")";
+        db.execSQL(sql);
+        // Dữ liệu mẫu giờ sẽ được chèn qua ProductDao.insertInitialSampleData()
+    }
+
 }
